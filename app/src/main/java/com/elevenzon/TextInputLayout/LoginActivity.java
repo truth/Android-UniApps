e1efb805e187574e7eb6d6f56342b24b5aab95bc
wangzhenli <wzl@gkmobile.com>
2021-04-10 00:13:09 +0800

    v1

---
+++ app/src/main/java/com/elevenzon/TextInputLayout/LoginActivity.java
@@ -0,0 +1,116 @@
+package com.elevenzon.TextInputLayout;
+
+import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Build;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.ActionBar;
+import android.util.Log;
+import android.util.Patterns;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.EditText;
+
+import com.smarx.notchlib.INotchScreen;
+import com.smarx.notchlib.NotchScreenManager;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText email, password;
+    Button login;
+    TextView register;
+    boolean isEmailValid, isPasswordValid;
+    TextInputLayout emailError, passError;
+    private static final String TAG = "landscape_notch";
+    private NotchScreenManager notchScreenManager = NotchScreenManager.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 设置Activity全屏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+// 隐藏ActionBar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        // 支持显示到刘海区域
+        notchScreenManager.setDisplayInNotch(this);
+        // 获取刘海屏信息
+        notchScreenManager.getNotchInfo(this, new INotchScreen.NotchScreenCallback() {
+            @Override
+            public void onResult(INotchScreen.NotchScreenInfo notchScreenInfo) {
+                Log.i(TAG, "Is this screen notch? " + notchScreenInfo.hasNotch);
+                if (notchScreenInfo.hasNotch) {
+                    for (Rect rect : notchScreenInfo.notchRects) {
+                        Log.i(TAG, "notch screen Rect =  " + rect.toShortString());
+                        // 将被遮挡的TextView左移
+//                        ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams) textView.getLayoutParams();
+//                        layoutParams.leftMargin = rect.right;
+//                        textView.setLayoutParams(layoutParams);
+                    }
+                }
+            }
+        });
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        register = (TextView) findViewById(R.id.register);
+        emailError = (TextInputLayout) findViewById(R.id.emailError);
+        passError = (TextInputLayout) findViewById(R.id.passError);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SetValidation();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // redirect to RegisterActivity
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void SetValidation() {
+        // Check for a valid email address.
+        if (email.getText().toString().isEmpty()) {
+            emailError.setError(getResources().getString(R.string.email_error));
+            isEmailValid = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()) {
+            emailError.setError(getResources().getString(R.string.error_invalid_email));
+            isEmailValid = false;
+        } else  {
+            isEmailValid = true;
+            emailError.setErrorEnabled(false);
+        }
+
+        // Check for a valid password.
+        if (password.getText().toString().isEmpty()) {
+            passError.setError(getResources().getString(R.string.password_error));
+            isPasswordValid = false;
+        } else if (password.getText().length() < 6) {
+            passError.setError(getResources().getString(R.string.error_invalid_password));
+            isPasswordValid = false;
+        } else  {
+            isPasswordValid = true;
+            passError.setErrorEnabled(false);
+        }
+
+        if (isEmailValid && isPasswordValid) {
+            Toast.makeText(getApplicationContext(), "Successfully", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+}
